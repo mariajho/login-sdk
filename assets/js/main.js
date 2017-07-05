14d45bdc0389c8840a095e2a2879de1eea9e12c6
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-07-05 12:30:56 -0400

    avance-ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+ window.fbAsyncInit = function() {
+ 	FB.init({
+ 		appId      : '322831918174183',
+ 		cookie     : true,
+ 		xfbml      : true,
+ 		version    : 'v2.8'
+ 	});
+ 	FB.AppEvents.logPageView();   
+ };
+
+ (function(d, s, id){
+ 	var js, fjs = d.getElementsByTagName(s)[0];
+ 	if (d.getElementById(id)) {return;}
+ 	js = d.createElement(s); js.id = id;
+ 	js.src = "//connect.facebook.net/en_US/sdk.js";
+ 	fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+
+ function checkLoginState() {
+ 	FB.getLoginStatus(function(response) {
+ 		statusChangeCallback(response);
+ 	});
+ }
