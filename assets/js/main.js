1c5a9bd3e11ab00434daad3e7033554d603afabf
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-07-05 13:39:43 -0400

    actualizando info del boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+ 
+
+
  window.fbAsyncInit = function() {
  	FB.init({
  		appId      : '322831918174183',
@@ -8,25 +11,7 @@
  	FB.AppEvents.logPageView();   
  };
 
- /*(function(d, s, id){
- 	var js, fjs = d.getElementsByTagName(s)[0];
- 	if (d.getElementById(id)) {return;}
- 	js = d.createElement(s); js.id = id;
- 	js.src = "//connect.facebook.net/en_US/sdk.js";
- 	fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));*/
-
-
- (function(d, s, id) {
- 	var js, fjs = d.getElementsByTagName(s)[0];
- 	if (d.getElementById(id)) return;
- 	js = d.createElement(s); js.id = id;
- 	js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=322831918174183";
- 	fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
-
- function checkLoginState() {
+  function checkLoginState() {
  	FB.getLoginStatus(function(response) {
  		statusChangeCallback(response);
  	});
